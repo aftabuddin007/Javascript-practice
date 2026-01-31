b2ce3d2d39e998ca18536bea3a298e3d1a98a8cb
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-01-31 23:37:03 +0600

    object practice done

---
+++ object.js
@@ -0,0 +1,25 @@
+
+
+
+
+// console.log(person.fname)
+// console.log(person.age)
+// person.age=33;
+// console.log(person)
+// let personKeys = Object.keys(person)
+// let personValue = Object.values(person)
+// let personEntries = Object.entries(person)
+
+// console.log(personKeys)
+// console.log(personValue)
+// console.log(personEntries)
+let person = {
+    fname:"kobir",
+    lName:"sing",
+    isDoctor:true,
+    age:21,
+
+}
+for(let entries of Object.entries(person)){
+    console.log(entries[0],' ',entries[1]);
+}
