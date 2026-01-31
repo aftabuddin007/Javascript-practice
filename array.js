8053c0230d6d5e5301284f53c1f5d56ef49f4461
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-01-31 23:58:34 +0600

    array practice done

---
+++ array.js
@@ -0,0 +1,14 @@
+// let name = ["aaa","bbb","ccc","ddd","eee"]
+
+// name.push("'fff")
+
+// console.log(name);
+
+// name.pop()
+// console.log(name.includes("aaa"));
+// console.log(name.indexOf("aaa"));
+
+// let name = ["aaa","bbb","ccc","ddd","eee"]
+// name.forEach(n => {
+//     console.log(n,": ok");
+// });
--- object.js
+++ object.js
@@ -20,6 +20,11 @@ let person = {
     age:21,
 
 }
-for(let entries of Object.entries(person)){
-    console.log(entries[0],' ',entries[1]);
-}
+// for(let entries of Object.entries(person)){
+//     console.log(entries[0],' ',entries[1]);
+// }
+// let personJson = JSON.stringify(person)
+// console.log(personJson);
+// let personRes = '{"fname":"kobir","lName":"sing","isDoctor":true,"age":21}'
+// let personObject = JSON.parse(personRes)
+// console.log(personObject);
