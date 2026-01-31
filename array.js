7132e56cb96b1c63fc588e9962932945245fdc12
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-02-01 00:33:18 +0600

    string practic done

--- array.js
+++ array.js
@@ -12,3 +12,13 @@
 // name.forEach(n => {
 //     console.log(n,": ok");
 // });
+// let number = [1,2,3,4,5,6,7,8,9,10]
+// let evenNum = number.filter((n)=> n%2 === 0)
+// console.log(evenNum);
+// let oddNum = number.filter((n)=> n%2 !== 0)
+// console.log(oddNum);
+// let sqrNum = number.map((n=>n*n))
+// console.log(sqrNum);
+let num = [23,44,65,12,78,99,70]
+let sortedValue = num.sort()
+console.log(sortedValue);
---
+++ string.js
@@ -0,0 +1,16 @@
+let message ="hello! Good Morning" ;
+// console.log(message.length);
+// console.log(message.toLocaleUpperCase());
+// console.log(message.toLocaleLowerCase());
+// console.log(message.indexOf("ing"));
+
+let currentDate = new Date()
+// console.log(currentDate);
+// console.log(currentDate.getFullYear());
+// console.log(currentDate.getMonth());
+// console.log(currentDate.getDay());
+// console.log(currentDate.toDateString());
+
+// console.log(Math.round(4.6));
+// console.log(Math.random());
+console.log(Math.floor(Math.random() * 6) +1);
