52af2fe55b41d008bbdbed69c6c6a3f34353a8c6
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-02-01 12:32:02 +0600

    make a calculator

---
+++ main.js
@@ -0,0 +1,58 @@
+let currentInput = ''
+let operator = ''
+let firstValue = ''
+
+function appendNumber(num){
+    currentInput = currentInput + num
+    document.getElementById('display').value = currentInput
+    // console.log(currentInput);
+}
+
+function operatorSymbol(op){
+  if(currentInput === "")return;  
+    firstValue = currentInput
+operator = op;
+currentInput =""
+
+
+
+
+}
+function calculate(){
+    if(firstValue===""||currentInput===""||operator==="")return
+let result ;
+let num1 = parseFloat(firstValue);
+let num2 = parseFloat(currentInput);
+switch (operator){
+    case "+":
+        result = num1+num2;
+        break;
+    case "-":
+        result = num1-num2;
+        break;
+    case "*":
+        result = num1*num2;
+        break;
+    case "/":
+        result = num1/num2;
+        break;
+    default:
+        return;
+}
+document.getElementById('display').value = result;
+currentInput=result.toString();
+firstValue="";
+operator="";
+
+
+
+
+
+
+}
+function clearDisplay(){
+    currentInput ="";
+    operator=""
+    firstValue=""
+    document.getElementById('display').value = "0"
+}
